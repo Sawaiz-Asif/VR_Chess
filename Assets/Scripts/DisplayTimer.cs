1d44921c38154c5c53548f377071a781f39bb34f
Mate Daniel Hajnal <mhajnal@cremi.emi.u-bordeaux1.fr>
2024-11-30 17:51:36 +0100

    Timer Code

---
+++ Assets/Scripts/DisplayTimer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayTimer : MonoBehaviour
+{
+    public TMP_Text textTimer;
+
+    private float timer = 600.0f;                                   // 10 minutes
+    private bool isTimer = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isTimer & timer >= 1.0f){
+            timer -= Time.deltaTime;
+            DisplayTime();
+            print(timer);
+        }
+    }
+
+    void DisplayTime(){
+        int minutes = Mathf.FloorToInt(timer / 60);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+
+        textTimer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    public void StartTimer(){
+        isTimer = true;
+    }
+
+    public void StopTimer(){
+        isTimer = false;
+        timer += 5.0f;                                  // 5sec increment
+        DisplayTime();
+    }
+
+}
